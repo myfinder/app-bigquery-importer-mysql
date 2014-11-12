132102d8fca5e8f28cbb04268b17a69ea86586ca
Tatsuro Hisamori <medianetworks@gmail.com>
2014-11-13 02:13:19 +0900

    rename module

--- lib/App/BigQuery/Impoter/MySQL.pm
+++ lib/App/BigQuery/Impoter/MySQL.pm
@@ -1,23 +1,41 @@
-# NAME
+package App::BigQuery::Impoter::MySQL;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01_1";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
 
 App::BigQuery::Impoter::MySQL
 
-# SYNOPSIS
+=head1 SYNOPSIS
 
     $ cpanm App::BigQuery::Impoter::MySQL
     $ mysqlbq -m ~/.my.cnf -b ~/.bigqueryrc --src SCHEMA_NAME.TABLE_NAME --dest DATASET_NAME.TABLE_NAME
 
-# DESCRIPTION
+
+=head1 DESCRIPTION
 
 App::BigQuery::Impoter::MySQL is BigQuery data importer from MySQL tables.
 
-# LICENSE
+=head1 LICENSE
 
 Copyright (C) Tatsuro Hisamori.
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself.
 
-# AUTHOR
+=head1 AUTHOR
 
-Tatsuro Hisamori <myfinder@cpan.org>
+Tatsuro Hisamori E<lt>myfinder@cpan.orgE<gt>
+
+=cut
+
--- t/00_compile.t
+++ t/00_compile.t
@@ -2,7 +2,7 @@ use strict;
 use Test::More 0.98;
 
 use_ok $_ for qw(
-    WebService::BigQuery::Impoter::MySQL
+    App::BigQuery::Impoter::MySQL
 );
 
 done_testing;
